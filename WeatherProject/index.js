bd8221d52a8434ba084d493426683cb7e5d3363b
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-26 21:00:31 +0530

    Initial commit

---
+++ WeatherProject/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const https= require("https");
+
+
+const app = express();
+
+
+app.get("/",function(req,res){
+    const url ="https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=8540560b0d13912db6ef32a2cca4458f";
+
+    https.get(url,function(resp){
+        console.log(resp);
+    })
+    res.send("Server is up and running");
+})
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
