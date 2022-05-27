2fc9abcac90aa57dd1d0873a3b43a83f865b1512
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-27 09:03:31 +0530

    Update index.js

--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -6,20 +6,25 @@ const app = express();
 
 
 app.get("/",function(req,res){
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=90&lon=90&appid=8540560b0d13912db6ef32a2cca4458f"
-    https.get(url,function(resp){
-        console.log(resp.statusCode);
+  res.sendFile(__dirname + "/index.html");
+//   const url = "https://api.openweathermap.org/data/2.5/weather?lat=90&lon=90&appid=8540560b0d13912db6ef32a2cca4458f"
+//     https.get(url,function(resp){
+//         console.log(resp.statusCode);
 
-        resp.on("data",function(data){
-        const weatherData = JSON.parse(data)
-        //    console.log(weatherData);
-        const temp = weatherData.main.temp
-        // console.log(temp);
-        res.send("The temperature of given place is   "+ temp);
-       }) 
-    })
+//         resp.on("data",function(data){
+//        const weatherData = JSON.parse(data)
+//         //    console.log(weatherData);
+//         const temp = weatherData.main.temp
+//         // console.log(temp);
+//         res.send("The temperature of given place is   "+ temp);
+//        }) 
+    // })
     // res.send("Server is up and running");
-})
+});
+
+app.post("/",function(req,res){
+    console.log(res.body.cityName);
+});
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
