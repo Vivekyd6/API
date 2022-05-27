569c88e579914d52bf1d0aad1ba7997ccf21368c
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-27 08:46:52 +0530

    Update index.js
    Fetching Api and displaying on browser

--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const https= require("https");
 
 
@@ -7,17 +6,19 @@ const app = express();
 
 
 app.get("/",function(req,res){
-    const url ="https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8540560b0d13912db6ef32a2cca4458f";
-
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=90&lon=90&appid=8540560b0d13912db6ef32a2cca4458f"
     https.get(url,function(resp){
         console.log(resp.statusCode);
 
-
-        resp.on("date",function(data){
-            console.log(data);
+        resp.on("data",function(data){
+        const weatherData = JSON.parse(data)
+        //    console.log(weatherData);
+        const temp = weatherData.main.temp
+        // console.log(temp);
+        res.send("The temperature of given place is   "+ temp);
        }) 
     })
-    res.send("Server is up and running");
+    // res.send("Server is up and running");
 })
 
 app.listen(3000,function(){
