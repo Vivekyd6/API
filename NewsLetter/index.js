fa4219ce8a61c6eaf8a7f52f9c542bc750c54330
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-31 14:27:11 +0530

    Added success and failure pages

--- NewsLetter/index.js
+++ NewsLetter/index.js
@@ -1,25 +1,77 @@
 const express = require("express");
-const bodyParser= require("body-parser");
+const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/",function(req,res){
-    var firstName = req.body.fname;
-    var lastName = req.body.lname;
-    var email = req.body.email;
+app.post("/", function (req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
 
-    console.log(firstName,lastName,email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us4.api.mailchimp.com/3.0/lists/53e60f0bb5";
+    const options = {
+        method: "post",
+        auth: "vivek:81e666c62775ebfd58899b17efc8a0ea-us14"
+    }
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function (data) {
+            // console.log(JSON.parse(data));
+        })
+    })
+
+
+    request.write(jsonData);
+    request.end();
+    console.log(firstName, lastName, email);
 })
-app.listen(3000,function(){
+
+app.post("/failure", function (req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 })
+
+
+
+
+
+
+
+
+
+
+// 81e666c62775ebfd58899b17efc8a0ea-us14
+
+// 53e60f0bb5.
