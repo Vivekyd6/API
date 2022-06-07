2cbc6cabc139b8e4c2313f2fa4a93ed7f51bb333
Vivek Yadav <vivekyadav36837@gmail.com>
2022-06-07 18:26:28 +0530

    Updated index.js,list.ejs & style.css
    Instead of storing the new item into array, used the mongodb method to store in database and then removing it from the database if not required.

--- TODOLIST_1/index.js
+++ TODOLIST_1/index.js
@@ -1,80 +1,106 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { use } = require("express/lib/application");
+// const { use } = require("express/lib/application");
 
 const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // var items = ["Buy Food","Cook Food","Get Food"];
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));  
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todoListDb",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/todoListDb", {
+  useNewUrlParser: true
+});
 
 const itemsSchema = new mongoose.Schema({
-  name : String
+  name: String
 });
 
-const Item = mongoose.model("Item",itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
-const item1= new Item({
-  name : "Welcome to your todolist"
+const item1 = new Item({
+  name: "Welcome to your todolist"
 });
 
-const item2= new Item({
-  name : "Hit the + button to add a new item"
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
 });
 
-const item3= new Item({
-  name : "Welcome to your todolist"
+const item3 = new Item({
+  name: "Welcome to your todolist"
 });
+const item4 = new Item({
+  name: "Welcome  your todolist"
+});
+
 
 const defaultItems = [item1,item2,item3];
 
-Item.insertMany(defaultItems,function(err){
-     if(err){
-       console.log(err);
-     }else{
-       console.log("Successfully Saved Default items to database ");
-     }
-})
+app.get("/", function (req, res) {
+      // res.sendFile(__dirname+"/index.html");
+      // javascript object :-  
+      // var today  = new Date();
+      // var currentDay = today.getDate();
+      // var day ="";
 
-app.get("/",function(req,res){
-    // res.sendFile(__dirname+"/index.html");
-    // javascript object :-  
-    // var today  = new Date();
-    // var currentDay = today.getDate();
-    // var day ="";
+      // if(currentDay==0 || currentDay==6){
+      //     day = "weekend";
+      //     // res.send("<h1>Its working day</h1>");
+      // }else {
+      //     day = "weekday"
+      //     // res.send("It's holiday");
+      // }
+      // var options ={
+      //   weekday : "long",
+      //   day:"numeric",
+      //   month:"long"
+      // };
+      Item.find({}, function (err, foundItems) {
+        if(foundItems.length===0){
+            Item.insertMany(defaultItems, function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Successfully Saved Default items to database ");
+            }
+          });
+          res.redirect("/");
+        }   else {
+          res.render("list", { kindOfDay: "Today", newListItems: foundItems });
+              // console.log(foundItems);
+              // console.log(foundItems.length);
+          }
+          });
 
-    // if(currentDay==0 || currentDay==6){
-    //     day = "weekend";
-    //     // res.send("<h1>Its working day</h1>");
-    // }else {
-    //     day = "weekday"
-    //     // res.send("It's holiday");
-    // }
-    var options ={
-      weekday : "long",
-      day:"numeric",
-      month:"long"
-    };
+        // var day = today.toLocaleDateString("en-Us",options);
+      });
 
-    // var day = today.toLocaleDateString("en-Us",options);
-    res.render("list",{kindOfDay:"Today", newListItems:defaultItems});
+    app.post("/delete",function(req,res){
+        console.log(req.body); 
+        const checkedItemId = req.body.checkbox;
+        Item.findByIdAndRemove(checkedItemId,function(err){
+          if(!err){
+            console.log("Successfully Deleted checked item.");
+            res.redirect("/");
+          } 
+        }) 
+    })
+    app.post("/", function (req, res) {
+       const  itemName = req.body.newItem;
+      const item = new Item({
+        name :itemName
+      });
+      item.save();
+      res.redirect("/");
+    })
 
-});
 
-app.post("/",function(req,res){
-   var item =  req.body.newItem;
-   items.push(item);
-   console.log(item);
-   res.redirect("/");
-})
-  
-
-app.listen(3000, function(){
-  console.log("server is running on port 3000");
-});
+    app.listen(3000, function () {
+      console.log("server is running on port 3000");
+    });
