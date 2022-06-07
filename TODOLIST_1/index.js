35eb770e0df2f7270b31112d3d365c8f8d4bb462
Vivek Yadav <vivekyadav36837@gmail.com>
2022-06-07 13:59:25 +0530

    use of database

--- TODOLIST_1/index.js
+++ TODOLIST_1/index.js
@@ -2,18 +2,50 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { use } = require("express/lib/application");
 
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine','ejs');
-var items = ["Buy Food","Cook Food","Get Food"];
+// var items = ["Buy Food","Cook Food","Get Food"];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));  
 
+mongoose.connect("mongodb://localhost:27017/todoListDb",{useNewUrlParser:true});
+
+const itemsSchema = new mongoose.Schema({
+  name : String
+});
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1= new Item({
+  name : "Welcome to your todolist"
+});
+
+const item2= new Item({
+  name : "Hit the + button to add a new item"
+});
+
+const item3= new Item({
+  name : "Welcome to your todolist"
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+     if(err){
+       console.log(err);
+     }else{
+       console.log("Successfully Saved Default items to database ");
+     }
+})
+
 app.get("/",function(req,res){
     // res.sendFile(__dirname+"/index.html");
     // javascript object :-  
-    var today  = new Date();
+    // var today  = new Date();
     // var currentDay = today.getDate();
     // var day ="";
 
@@ -30,8 +62,8 @@ app.get("/",function(req,res){
       month:"long"
     };
 
-    var day = today.toLocaleDateString("en-Us",options);
-    res.render("list",{kindOfDay:day, newListItems:items});
+    // var day = today.toLocaleDateString("en-Us",options);
+    res.render("list",{kindOfDay:"Today", newListItems:defaultItems});
 
 });
 
@@ -43,6 +75,6 @@ app.post("/",function(req,res){
 })
   
 
-app.listen(3001, function(){
-  console.log("server is running on port 3001");
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
 });
